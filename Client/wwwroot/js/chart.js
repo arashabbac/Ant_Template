387934f89ee3efec375907696500f646f32778c6
Arash Abs <araash.abs@gmail.com>
2021-07-01 20:33:34 +0430

    add apex chart

--- Client/wwwroot/js/chart.js
+++ Client/wwwroot/js/chart.js
@@ -1,116 +1,49 @@
-﻿function ReportChart(datas , labels , chartTitle) {
-    $(document).ready(function () {
-        //=======================            Income BarChart               =========================//
-        var options = {
-            series: [{
-                name: chartTitle,
-                data: datas
-            },],
-            responsive: [
-                {
-                    breakpoint: 450,
-                    options: {
-                        //plotOptions: {
-                        //    bar: {
-                        //        horizontal: false
-                        //    }
-                        //},
-                        //legend: {
-                        //    position: "bottom"
-                        //},
-                        xaxis: {
-                            labels: {
-                                show: false,
-                            }
-                        },
-                        //chart: {
-                        //    width:300,
-                        //    height: 305,
-                        //},
-
-                    }
-                }
-            ],
-            chart: {
-                type: 'bar',
-                height: 305
+﻿function BarChartFunction() {
+    var options = {
+        series: [{
+            name: 'Net Profit',
+            data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+        }, {
+            name: 'Revenue',
+            data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        }, {
+            name: 'Free Cash Flow',
+            data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+        }],
+        chart: {
+            type: 'bar',
+            height: 350
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: '55%',
+                endingShape: 'rounded'
             },
-            plotOptions: {
-                bar: {
-                    horizontal: false,
-                    columnWidth: '35%',
-                    endingShape: 'flat'
-                },
-            },
-            dataLabels: {
-                enabled: false
-            },
-            stroke: {
-                show: true,
-                width: 1,
-                colors: ['transparent']
-            },
-            colors: ['#05c46b', '#e74c3c', '#2980b9'],
-            xaxis: {
-                categories: labels,
-                labels: {
-                    style: {
-                        fontSize: '12px',
-                        fontFamily: 'yekan',
-                        fontWeight: 500,
-                    },
-                }
-            },
-            yaxis: {
-                type: "category",
-                title: {
-                    text: ' (تعداد)',
-                    rotate: 360,
-                    offsetX: 12,
-                    offsetY: -125,
-                    style: {
-                        color: '#78909c',
-                        fontSize: '10px',
-                        fontFamily: 'yekan',
-                        fontWeight: 500,
-                    },
-                },
-                tickAmount: 2,
-                labels: {
-                    align: 'center',
-                    style: {
-                        fontFamily: 'yekan',
-                        fontSize: 12,
-                        fontWeight: 600,
-                    },
-                }
-            },
-            legend: {
-                fontSize: '12px',
-                fontFamily: 'yekan',
-                fontWeight: 400,
-                markers: {
-                    radius: 15,
-                    strokeWidth: 1,
-                },
-            },
-            fill: {
-                opacity: 1
-            },
-            tooltip: {
-                style: {
-                    fontSize: '12px',
-                    fontFamily: 'yekan',
-                },
-                y: {
-                    formatter: function (val) {
-                        return val.toString();
-                    },
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+        },
+        xaxis: {
+            categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        },
+        fill: {
+            opacity: 1
+        },
+        tooltip: {
+            y: {
+                formatter: function (val) {
+                    return "$ " + val + " thousands"
                 }
             }
-        };
+        }
+    };
 
-        var chart = new ApexCharts(document.querySelector("#reportChart"), options);
-        chart.render();
-    });
+    var chart = new ApexCharts(document.querySelector("#barChart"), options);
+    chart.render();
 }
